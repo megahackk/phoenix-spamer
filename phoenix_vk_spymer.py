4255ca7d6517bb47e979c5272ab6083b6f6d06dd
megahackk <90929506+megahackk@users.noreply.github.com>
2021-10-18 23:57:07 +0300

    Update phoenix_vk_spymer.py

--- phoenix_vk_spymer.py
+++ phoenix_vk_spymer.py
@@ -1,45 +1,203 @@
-import vk_api
-import time
+try:
+	import time
+	import os
+	import vk_api
+except:
+	i=print("[Для работы нужно установить модули]")
+	pass
+
+
+try:
+	print(i)
+except:
+	i=" "
+else:
+	pass
+
 
 print(" ")
 print(" ")
-print(" ")
-print(" ")
-print(" ")
-print(" ")
 
-
-
-tok=input("введите токен:")
-count=input("введите переменную для работы с параметром рандом айди:")
-num=input("введите номер:")
-count=int(count)
 p=1
+count=1
 
-ses=vk_api.VkApi(token=tok)
-vk=ses.get_api()
+if i=="[Для работы нужно установить модули]":
+	pass
+else:
+	i=""
 
-def sen(text):
+
+def send(text,d):
 	global count
+	if d==1:
+		vk1.messages.send(peer_id=-94631511,message=text, random_id=count )
+		count=count+1	
+	elif d==2:
+		vk2.messages.send(peer_id=-94631511,message=text, random_id=count )
+		count=count+1
 
-	vk.messages.send(peer_id=-94631511,message=text, random_id=count )
-	count=count+1
+def doses():
+	global vk1
+	global vk2
+	global ses1
+	global ses2
 
-def spam():
+	if work_mod==0:
+		ses1=vk_api.VkApi(token=tok1)
+		vk1=ses1.get_api()
+	else:
+		ses2=vk_api.VkApi(token=tok2)
+		vk2=ses2.get_api()
+		ses1=vk_api.VkApi(token=tok1)
+		vk1=ses1.get_api()
+		
+
+
+def GetToks():
+	global tok1
+	global tok2
+	global work_mod
+	global i
+	tok1=input("Введите первый токен: ")
+	a=input("Второй токен?  Y/N ")
+	if a=="n":
+		work_mod=0
+	elif a=="y":
+		tok2=input("Введите второй токен: ")
+		work_mod=1
+	else:
+		i="[Неверный ввод токенов]"
+	doses()
+	i="[Токен/ы введен/ы]"
+	main()
+
+def GetNum():
+	global i
+	global num
+	num=input("Введите номер:")
+	i="[Номер установлен]"
+	main()
+
+def GetTime():
+	global i
+	global timeq
+	print("1 час-3600 секунд")
+	timeq=input("Введите время")
+	i="[Время в "+ str(timeq) +" секунд установлено]"
+	main()
+
+
+def start():
+	global i
+	print("")
+	try:
+		timeq=timeq+timeq
+	except:
+		timeq=0
+	else:
+		pass
+	print("[Работа...]")
+	time.sleep(timeq)
+	if work_mod==0:
+		onetok()
+	elif work_mod==1:
+		twotok()
+	main()
+
+
+
+
+def onetok():
+	global num
 	global p
 	try:
-		sen(num)
+		send(num,1)
 		time.sleep(9)
-		sen("Ввести другой номер телефона")
+		send("Ввести другой номер телефона",1)
 	except:
-		print("error")
+		print("[Ошибка...]")
 		time.sleep(9)
-		spam()
+		onetok()
 	else:
-		p=str(p)
-		print("запрос отправлен" " " +p)
+		print("[Запрос отправлен "  +str(p)+"]")
 		time.sleep(9)
-		p=int(p)
 		p=p+1
-		spam()
-spam()
+		onetok()
+
+
+
+
+def twotok():
+	global num
+	global p
+	try:
+		send(num,1)
+		time.sleep(9)
+		send("Ввести другой номер телефона",1)
+		send(num,2)
+		time.sleep(9)
+		send("Ввести другой номер телефона",2)
+	except:
+		print("[Ошибка...]")
+		time.sleep(9)
+		twotok()
+	else:
+		print("[Запрос отправлен "  +str(p)+"]")
+		print("[Запрос отправлен "  +str(p+1)+"]")
+		p=p+2
+		twotok()		
+
+def moduls():
+	global i
+	print("[Загрузка...]")
+	os.system("pip install vk_api")
+	i="[Модуль загружен]"
+	main()
+
+def upd():
+	global i
+	func=input("Вы уверены что хотите обновить Y/N ")
+	if func=="y":
+		os.system("cd && rm -rf phoenix-spamer && git clone https://github.com/megahackk/phoenix-spamer && cd phoenix-spamer && python3 phoenix_vk_spymer.py")
+	elif func=="n":
+		i="[Обновление отменено]"
+	else:
+		print("")
+		print("[Неверный ввод]")
+		print(" ")
+		upd()
+
+
+def main():
+	global i
+	os.system('cls' if os.name=='nt' else 'clear')
+	print(i)
+	print("Phoenix spamer by vk_api")
+	print("1;{[Вводы токена}]")
+	print("2;{[Ввод номерa}]")
+	print("3;{[Установка таймера}]")
+	print("4;{[Мой ютуб канал}]")
+	print("5;{[Запуск спамера}]")
+	print("6;{[Установка модулей}]")
+	print("7;{[Обновление}]")
+	print("")
+	func=input("Что будем делать: ")
+	if   func==str(1):
+		GetToks()
+	elif func==str(2):
+		GetNum()
+	elif func==str(3):
+		GetTime()
+	elif func==str(4):
+		print("")
+		print("https://www.youtube.com/channel/UCC0-w64BrjxtcFfLE9wAJBQ")
+	elif func==str(5):
+		start()
+	elif func==str(6):
+		moduls()
+	elif func==str(7):
+		upd()
+	else:
+		print("")
+		main()
+main()
